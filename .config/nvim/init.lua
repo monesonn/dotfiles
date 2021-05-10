0746e3bf553a29ebc3b50b7e87f302bef9b993c5
monesonn <monesonn@tuta.io>
2021-05-10 12:40:53 +0300

    nvim: migrating to lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,19 @@
+--[[
+   _      _ __    __
+  (_)__  (_) /_  / /_ _____ _
+ / / _ \/ / __/ / / // / _ `/
+/_/_//_/_/\__(_)_/\_,_/\_,_/
+                nvim >= 0.5
+
+]]
+
+-- About:  Configuration files for Neovim
+-- Author: Stanislav <git.io/monesonn>
+-- Link:   http://git.io/.mn
+
+vim.cmd('source ~/.config/nvim/vimscript/general.vim')
+vim.cmd('source ~/.config/nvim/vimscript/plugins.vim')
+vim.cmd('source ~/.config/nvim/vimscript/plugins-settings.vim')
+vim.cmd('source ~/.config/nvim/vimscript/ui.vim')
+vim.cmd('source ~/.config/nvim/vimscript/shortcuts.vim')
+vim.cmd('source ~/.config/nvim/vimscript/functions.vim')
